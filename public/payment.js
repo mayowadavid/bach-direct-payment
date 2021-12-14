5f925b6ec494203fc56b2d6c3c2d517077383959
mayowadavid <34623610+mayowadavid@users.noreply.github.com>
2021-12-14 04:23:25 -0800

    Update payment.js

--- public/payment.js
+++ public/payment.js
@@ -65,12 +65,10 @@ fetch('/config')
     return result.json();
   })
   .then(function (json) {
-  console.log(json);
     var stripe = Stripe(json.publicKey);
     // Setup event handler to create a Checkout Session on submit
     document.querySelector('#payNow').addEventListener('click', function (evt) {
       createCheckoutSession().then(function (data) {
-          console.log(data);
         stripe
           .redirectToCheckout({
             sessionId: data.sessionId,
