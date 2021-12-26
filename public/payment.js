2608a22070aeb33256e60dfc654b09761ad52fc2
mayowadavid <34623610+mayowadavid@users.noreply.github.com>
2021-12-26 07:17:00 -0800

    Update payment.js

--- public/payment.js
+++ public/payment.js
@@ -23,42 +23,11 @@ var createCheckoutSession = function () {
     body: JSON.stringify({
       quantity: quantity,
       locale: i18next.language.toLowerCase().split('-')[0],
-      price: selectedPrice,
     }),
   }).then(function (result) {
     return result.json();
   });
 };
-let priceList;
-/* fetch prices */
-const prices = async () => {
-    $("#rotate").toggle();
-    let {priceData} = await fetch('price-fetch', { method: 'GET'}).then(function (result) {
-        return result.json();
-      });
-    priceList = priceData;
-    if(priceData){
-        $("#rotate").toggle();
-        priceData.map( async ({unit_amount})=> {
-            $("#price_option").append("<option>"+ 
-            "£" + unit_amount/100 
-            + "</option>") 
-            })
-    }
-}
-const price_fetch = document.getElementById('price_pool');
-    price_fetch.addEventListener('click', async (event) => {
-        prices();
-    })
-const selected_price = document.getElementById('price_option');
-selected_price.addEventListener('change', async (event) => {
-        event.preventDefault();
-        let selected;
-        selected = event.target.value;
-        let priceNum = parseFloat(selected.replace(/£/g, ""));
-        let returnPriceId = priceList.find(e => e.unit_amount/100 == priceNum );
-        selectedPrice = returnPriceId.id;
-      });  
 
 /* Get your Stripe publishable key to initialize Stripe.js */
 fetch('/config')
@@ -66,7 +35,8 @@ fetch('/config')
     return result.json();
   })
   .then(function (json) {
-    var stripe = Stripe(json.publicKey);
+    window.config = json;
+    var stripe = Stripe(config.publicKey);
     // Setup event handler to create a Checkout Session on submit
     document.querySelector('#payNow').addEventListener('click', function (evt) {
       evt.preventDefault();
